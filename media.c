8baeeb38bb48986bee1cc8fd7b46b83b580045a6
PauloHARocha <ph.alcantara.rocha@gmail.com>
2026-02-12 09:00:05 -0300

    Add media unicap program

---
+++ media.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+    float nota_1GQ = 8.0;
+    float nota_2GQ = 5.0; 
+
+    float media = (nota_1GQ*2 + nota_2GQ*3)/5;
+
+    printf("Media: %.1f \n", media);
+    
+    return 0;
+}

fdf4128e642bd690be2a3f04409fcd09cd389205
PauloHARocha <ph.alcantara.rocha@gmail.com>
2026-02-19 08:45:03 -0300

    Update media unicap with scanf

--- media.c
+++ media.c
@@ -1,13 +1,25 @@
 #include<stdio.h>
 
 int main(){
+    float nota_1GQ, nota_2GQ, media;
 
-    float nota_1GQ = 8.0;
-    float nota_2GQ = 5.0; 
+    printf("Nota 1GQ: ");
+    scanf("%f", &nota_1GQ);
+    printf("Nota 2GQ: ");
+    scanf("%f", &nota_2GQ);
 
-    float media = (nota_1GQ*2 + nota_2GQ*3)/5;
+    media = (nota_1GQ*2 + nota_2GQ*3)/5;
+    printf("Media: %.2f \n", media);
 
-    printf("Media: %.1f \n", media);
+    if(media >= 7){
+        printf("Aprovado\n");
+    }else{
+        if(media >= 3){
+            printf("Final\n");
+        }else{
+            printf("Reprovado\n");
+        }
+    }
     
     return 0;
 }

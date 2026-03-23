aeb08c89adef8a4ce480f340c398dd254c27ccc5
PauloHARocha <ph.alcantara.rocha@gmail.com>
2026-03-23 08:32:44 -0300

    Add exemplo vetores e ponteiros

---
+++ material-aulas/em-sala/vetores_5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+// calcule o dobro de cada posicao 
+// do vetor e armezene na posicao 
+// receber o vetor
+void dobro(int* a, int n){ // int a[]
+    for(int i = 0; i < n; i++){
+        a[i] = a[i]*2;
+    }
+}
+int main(){
+    int n = 5;
+    int v[] = {10, 30, 20, 5, 2};
+
+    printf("v: %p\n", v);
+    dobro(v, n);
+
+    for(int i=0;i<n;i++){
+        printf("v[%d]: %d | &v[%d]: %p\n", i, *(v + i), i, (v + i));
+    }
+
+    return 0;
+}
